8ccc6e2a68c6641b54d440321f8ed7784d7f5da2
nataliyaIgnatova <117376648+nataliyaIgnatova@users.noreply.github.com>
2022-12-11 19:56:08 +0000

    add new Exercise

---
+++ JavaFundamentals/01BasicSyntaxConditionalStatementsAndLoops/Exercise/BasicSynExercise/src/e4PrintAndSum.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class e4PrintAndSum {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int start = Integer.parseInt(scanner.nextLine());
+        int end = Integer.parseInt(scanner.nextLine());
+
+        int sum = 0;
+        for (int i = start; i <= end ; i++) {
+
+            sum+=i;
+
+            System.out.printf("%d ",i);
+
+        }
+        System.out.printf("%nSum: %d",sum);
+    }
+}
