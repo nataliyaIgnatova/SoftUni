2df79b6767d98416affdcf17e1b41f123a3951a6
nataliyaIgnatova <117376648+nataliyaIgnatova@users.noreply.github.com>
2022-12-18 17:21:25 +0000

    add exercise 9,10,11 Types and Variables

--- JavaFundamentals/02Data Types and Variables/exe/Types and Variables/src/e9SpiceMustFlow.java
+++ JavaFundamentals/02Data Types and Variables/exe/Types and Variables/src/e9SpiceMustFlow.java
@@ -1,2 +1,28 @@
+import java.util.Scanner;
+
 public class e9SpiceMustFlow {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        int yieldStartingSource = Integer.parseInt(scanner.nextLine());
+
+        int daysCount = 0;
+        int workersConsume = 0;
+        int spiceExtracted = 0;
+
+        for (int sum = yieldStartingSource; sum >= 100 ; sum -= 10) {
+
+            daysCount++;
+            int sumWorkers = sum - 26;
+            spiceExtracted +=sumWorkers  ;
+        }
+        System.out.println(daysCount);
+        System.out.printf("%d", spiceExtracted - 26);
+
+
+
+    }
+
 }
