3e8d8aaf94f61a712e684b428f7e4986a73375f0
nataliyaIgnatova <117376648+nataliyaIgnatova@users.noreply.github.com>
2023-10-16 07:13:02 +0100

    Java Basic

---
+++ JavaFundamentals/List/src/Train.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Train {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+
+
+    }
+}
