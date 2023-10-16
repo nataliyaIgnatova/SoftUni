70f4bc3f5cf69a580f0968cbbcabd27d8812c6f4
nataliyaIgnatova <117376648+nataliyaIgnatova@users.noreply.github.com>
2023-10-16 07:10:31 +0100

    Java Basic

---
+++ JavaFundamentals/List/src/e02GaussTrick.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class e02GaussTrick {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> numbers = Arrays
+                .stream(scanner.nextLine().split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+
+        for (int i = 0; i < numbers.size()/2 ; i++) {
+
+            numbers.set(i,(numbers.get(i) + numbers.get(numbers.size()-1)));
+            numbers.remove(numbers.size()-1);
+
+        }
+
+
+        for (int number: numbers) {
+
+            System.out.print(number + " ");
+
+
+        }
+    }
+
+
+}
