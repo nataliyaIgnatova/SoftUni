d0293decc97addc6cc978240693f430e6a44483e
nataliyaIgnatova <117376648+nataliyaIgnatova@users.noreply.github.com>
2024-03-19 00:01:10 +0000

    Java Advanced.iml

---
+++ Java Advanced/Stack and Queues Lab/src/e03DecimalToBinaryConverter.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class e03DecimalToBinaryConverter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
